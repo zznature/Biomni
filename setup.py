67e542a695829f2fa7bb86ca1fdeea87d675b301
kexinhuang12345 <kexinh@stanford.edu>
2025-05-12 12:57:12 -0700

    add all

--- setup.py
+++ setup.py
@@ -17,12 +17,6 @@ def readme():
     with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
         return f.read()
 
-
-# read the contents of requirements.txt
-with open(path.join(this_directory, 'requirements.txt'),
-          encoding='utf-8') as f:
-    requirements = f.read().splitlines()
-
 setup(name='biomni',
       version=__version__,
       license='MIT',
@@ -35,6 +29,5 @@ setup(name='biomni',
       packages=find_packages(exclude=['test']),
       zip_safe=False,
       include_package_data=True,
-      install_requires=requirements,
       setup_requires=['setuptools>=38.6.0']
       )
